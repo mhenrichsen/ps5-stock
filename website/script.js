f76ba32b8726bf68d0498f509845920e2b2a904c
Nicolaj Hammerbo <nicolaj@hammerbo.com>
2021-01-12 18:27:20 +0100

    added datatables, changed html to work with it. Added colors to show stock status

--- website/script.js
+++ website/script.js
@@ -4,57 +4,42 @@ const head = ["Butik", "Model", "Lagerstatus", "Sidst opdateret"];
 
 window.addEventListener("load", function () {
     const data = '';
-    fetch('/status')
+    fetch('/status.json')
         .then(response => response.json())
         .then(function(data) {
-            let thead = table.createTHead();
-            let row = thead.insertRow();
 
-            for (key of head) {
-                console.log(key);
-                let th = document.createElement("th");
-                let text = document.createTextNode(key);
-                th.appendChild(text);
-                row.appendChild(th);
+            console.log(data);
+
+            var table = $('table').DataTable({
+                columns: [{ data: "store" }, 
+                { data: "model" }, 
+                { data: "status" },
+                { data: "lastupdated" }],
+                order: [[1, "desc"], [2, "desc"]],
+                paging: false
+            });
+            for(var k of Object.entries(data))
+            {
+                const product = k[1];
+                let row = table.row.add({
+                    "store": product.store,
+                    "model": product.product_name ,
+                    "status": '<a href="' + product.product_url + '">' + product.stock + '</a>',
+                    "lastupdated": product.time}).draw().node();
+
+                if(product.stock !== 'Ikke på lager')
+                    $(row).addClass('inStock');
+                else 
+                    $(row).addClass('notInStock');
             }
-
-            for (const i of Object.entries(data)) {
-                const product_url = i[1]["product_url"];
-                const product_name = i[1]["product_name"];
-                const store = i[1]["store"];
-                const time = i[1]["time"];
-                const stock = i[1]["stock"];
-                let therow = [store, product_name, stock, time];
-                let row = table.insertRow();
-                for (ce of therow) {
-                    if (ce.includes('lager')) {
-                        console.log('link found');
-                        let cell = row.insertCell();
-                        let link = document.createElement("a");
-                        var href = product_url;
-                        link.href = href;
-                        link.innerHTML = ce;
-                        cell.appendChild(link);
-                    } else {
-                        let cell = row.insertCell();
-                        let text = document.createTextNode(ce);
-                        cell.appendChild(text);
-                    }
-                }
-
-                console.log(product_url, store, time, stock)
-            }
-
-
-
-        });
-
+            
+    });     
 });
 document.forms['form-email'].addEventListener('submit', (event) => {
     event.preventDefault();
     const email = document.getElementById('email-adress').value;
 
-    console.log(email);
+    // console.log(email);
     fetch('/add-email?email=' + email)
         .then(response => response.json())
         .then(document.getElementById('emailHelp').innerHTML = "Vi har tilføjet din email adresse");
